7908d2f784090e3f06a5703f22e846dc5c89a6aa
A-S620 <aatikaseedat00@gmail.com>
2022-10-06 20:56:22 +0100

    added to USMedicalInsuranceCosts.ipynb

---
+++ notebook/Projects/shared/MetricsHTML.py
@@ -0,0 +1,34 @@
+style = '''
+<style> 
+#metrics {
+    width: 95%;
+    margin: 20px auto;
+}
+.box {
+    background-color: #9056FA;
+    height: 100px;
+    width: 200px;
+    margin: 20px;
+    font-weight: bold;
+    color:#FFFFFF;
+    text-align: center;
+    display: inline-block;
+    float: left;
+}
+.head {
+    padding: 15px;
+    font-size: 30px;
+    line-height: 35px;
+}
+</style>
+'''
+html = '''
+<div id="metrics">
+    <div class="box">
+        <div class="head">{}</div>
+    </div>
+    <div class="box">
+        <div class="head">{}</div>
+    </div>
+</div>
+'''

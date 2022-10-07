1b4a7a81c1568499e0035528cb7cfa660c105eef
A-S620 <aatikaseedat00@gmail.com>
2022-10-07 20:36:41 +0100

    trying to make the metrics look cute

--- notebook/Projects/shared/MetricsHTML.py
+++ notebook/Projects/shared/MetricsHTML.py
@@ -1,34 +1,47 @@
 style = '''
 <style> 
+body {
+    background-color: #000000
+}
 #metrics {
     width: 95%;
     margin: 20px auto;
-}
-.box {
-    background-color: #fecef1;
-    height: 100px;
-    width: 200px;
-    margin: 20px;
-    font-weight: bold;
-    color:#FFFFFF;
-    text-align: center;
-    display: inline-block;
-    float: left;
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
 }
 .head {
-    padding: 15px;
+    padding: 5px;
     font-size: 30px;
-    line-height: 35px;
+    color: #32292f;
+}
+.sub-text {
+    padding: 5px;
+    font-size: 20px;
+    color: #32292f;
+}
+.container {
+  border-radius: 15px 50px;
+  background: #89a1ef;
+  padding: 20px;
+  width: 200px;
+  height: 150px;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
 }
 </style>
 '''
 
 html = '''
 <div id="metrics">
-    <div class="box">
+    <div class="container">
+        <div class="sub-text">{}</div>
         <div class="head">{}</div>
     </div>
-    <div class="box">
+    <div class="container">
+        <div class="sub-text">{}</div>
         <div class="head">{}</div>
     </div>
 </div>
